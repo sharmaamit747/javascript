d17c3a79ce5e32a03dd7d0df283ff611d730938a
Amit Kumar <sharmaamit747@gmail.com>
2025-06-12 07:33:42 +0000

    datatypes

--- .devcontainer/01_basics/02_datatypes.js
+++ .devcontainer/01_basics/02_datatypes.js
@@ -2,4 +2,21 @@
 
 // alert(3 + 3) // we are using node js not js
 
- 
+ let name = "Amit"
+ let age = 35
+ let isLoggedIn = false
+
+ // number => 2 to the power 53
+ // bigInt
+ // string => ""
+ // boolean => true/flase
+ // null => standalone value
+ // undefined => 
+// symbol => unique
+  
+ // object
+
+console.log(typeof "Amit");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
