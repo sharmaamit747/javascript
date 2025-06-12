3270555283e173b9b26371c5ee6f22a15d29a1d6
Amit Kumar <sharmaamit747@gmail.com>
2025-06-12 07:13:34 +0000

    variable and constant

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 9985455
+let accountName ="Amit Kumar"
+var accountPassword = 'dfd5454'
+accountCity = 'Jaipur'
+let accountState;
+
+// accountId = 95798 // constant never changed
+
+accountName ="Amit Rajbeer Kumar"
+accountPassword = '4447ddd'
+accountCity = 'Delhi'
+
+
+
+/* Prefered not to use'var' bcoz block scope and functional scope */
+
+console.log(accountId)
+console.table([accountId, accountName, accountPassword, accountCity, accountState])
