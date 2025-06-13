9b492c35c0a2eafe3e2ebe6f55c9a8bb4bb7f047
Amit Kumar <sharmaamit747@gmail.com>
2025-06-13 05:46:41 +0000

    comparision done

---
+++ .devcontainer/01_basics/04_comparision.js
@@ -0,0 +1,11 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null <= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+
+// avoid these type comparisions
