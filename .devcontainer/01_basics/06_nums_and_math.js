a595cd6865e908a986a03bb574a500df7d922869
Amit Kumar <sharmaamit747@gmail.com>
2025-06-16 06:50:59 +0000

    maths and nums

---
+++ .devcontainer/01_basics/06_nums_and_math.js
@@ -0,0 +1,32 @@
+const score = 400
+// console.log(score)
+
+const balance = new Number(100)
+
+// console.log(balance.toFixed(2))
+
+// console.log(balance.toPrecision(2))
+
+const hunderds = 1000000
+
+// console.log(hunderds.toLocaleString())
+// console.log(hunderds.toLocaleString('en-in'))
+
+
+//---------------------------------------Maths-----------------------------------------------
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(44.654))
+// console.log(Math.ceil(44.654))
+// console.log(Math.floor(44.654))
+// console.log(Math.min(4,37,1))
+
+console.log(Math.random())
+console.log((Math.random()*10) + 1)
+console.log((Math.random()*10) + 1)
+
+const min = 1
+const max = 6
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
