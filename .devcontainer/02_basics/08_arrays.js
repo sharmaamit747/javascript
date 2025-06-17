d318dcaff31c97f97670dd7c67a5ac7477c359c7
Amit Kumar <sharmaamit747@gmail.com>
2025-06-17 05:32:45 +0000

    array part 1

---
+++ .devcontainer/02_basics/08_arrays.js
@@ -0,0 +1,28 @@
+const myArr = [0,1,2,3,4,5,6]
+// console.log(myArr)
+
+// console.log(myArr.push(7))
+
+// console.log(myArr.length)
+
+// console.log(myArr.pop())
+
+// console.log(myArr.toString())
+
+// console.log(myArr.at(3))
+
+// console.log(myArr.join(' * '))
+
+// console.log(myArr.shift())
+// console.log(myArr.shift())
+// console.log(myArr.unshift(8))
+
+const newArr = ['ram', 'Krishan']
+const newArr2 = ['sita', 'gita']
+
+// console.log(myArr.concat(newArr, newArr2))
+
+const myArr5 = [[1,2],[3,4],[5,6]];
+const newArr5 = myArr5.flat();
+
+console.log(newArr5)
