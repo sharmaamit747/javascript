779f33eb3160aaebd70df671ec4d94cdde4ee8dc
Amit Kumar <sharmaamit747@gmail.com>
2025-06-19 11:51:04 +0000

    function first

---
+++ .devcontainer/03_basics/01_functions.js
@@ -0,0 +1,11 @@
+function multiply(number1 = 1, number2 = 1){
+    if(!number1 || !number2){
+        console.log("to few arguments")
+        return
+    }
+    return number1 * number2
+}
+
+// const result = multiply(5, 10255)
+// const result = multiply(10255)
+console.log(result)
