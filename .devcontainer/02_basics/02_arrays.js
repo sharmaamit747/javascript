740b5877a43d5d9525c105b3c38104a39eb3f19e
Amit Kumar <sharmaamit747@gmail.com>
2025-06-19 06:44:17 +0000

    array complete

---
+++ .devcontainer/02_basics/02_arrays.js
@@ -0,0 +1,20 @@
+const marvel_heros = ['Hulk', 'Caption America', 'SpiderMen']
+const dc_heros = ['batman', 'shaktiman', 'junierji']
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros)
+
+// const allheros = marvel_heros.concat(dc_heros)
+
+// const allheros = [...marvel_heros, ...dc_heros] (Note: ...spread operator)
+
+
+// console.log(allheros)
+
+// const unflat = [1, 2, 3, [4, 5, 6, [7, 5 , 4], [2, 1]], 9, 11]
+
+// console.log(unflat.flat(3))
+
+// console.log(Array.isArray([5]))
+
+console.log(Array.from(5))
